bba688ac7b7bab27cb4a478921b9b7c42e20ac76
Musa Hamwala II <musahamwala@icloud.com>
2019-11-12 01:48:43 +0000

    Adding Inital MVC for Staff

---
+++ ThreeAmigosStaff/ThreeAmigosStaff/Models/MvcStaffContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcStaff.Models
+{
+    public class MvcStaffContext : DbContext
+    {
+        public MvcStaffContext(DbContextOptions<MvcStaffContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staff> Staff { get; set; }
+    }
+}
