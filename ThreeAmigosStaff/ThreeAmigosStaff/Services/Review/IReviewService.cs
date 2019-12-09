c2e92a158f32adf8f91c2cc6a9fd57fda572e071
Musa Hamwala II <musahamwala@icloud.com>
2019-12-09 13:30:01 +0000

    reviews displayed but not picking up specific reviews

---
+++ ThreeAmigosStaff/ThreeAmigosStaff/Services/Review/IReviewService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ThreeAmigosPurchase.Services;
+
+namespace ThreeAmigosReview.Services
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<ReviewDto>> GetReviewAsync();
+
+        Task<IEnumerable<ReviewDto>> GetReviewsAsync(int Id);
+
+        Task<ReviewDto> GetReviewDetailsAsync(int Id);
+
+        Task<IEnumerable<ReviewDto>> GetReviewDetailsListAsync(int Id);
+
+        Task<ReviewDto> PostReviewAsync(ReviewDto ReviewDto);
+
+        Task<ReviewDto> EditReviewDetailsAsync(int Id);
+
+        Task<ReviewDto> GetDeleteReviewAsync(int Id);
+    }
+}
---
+++ ThreeAmigosStaff/ThreeAmigosStaff/Services/Review/ReviewService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ThreeAmigosPurchase.Services;
+
+namespace ThreeAmigosReview.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly HttpClient _client;
+
+        public ReviewService(HttpClient client)
+        {
+            client.BaseAddress = new System.Uri("http://localhost:5001/");
+            client.Timeout = TimeSpan.FromSeconds(5);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            _client = client;
+        }
+
+        //Get all Review
+        public async Task<IEnumerable<ReviewDto>> GetReviewAsync()
+        {
+            var response = await _client.GetAsync("Review/");
+            if(response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<IEnumerable<ReviewDto>>();
+            return review;
+
+        }
+
+        //Get all Reviews
+        public async Task<IEnumerable<ReviewDto>> GetReviewsAsync(int Id)
+        {
+            var response = await _client.GetAsync("Review/" + Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<IEnumerable<ReviewDto>>();
+            return review;
+        }
+
+        //Get Individual Review Details
+        public async Task<ReviewDto> GetReviewDetailsAsync(int Id)
+        {
+            var response = await _client.GetAsync("Review/details/" + Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<ReviewDto>();
+            return review;
+        }
+
+        //Post New Review Member
+        public async Task<ReviewDto> PostReviewAsync(ReviewDto reviews)
+        {
+            var response = await _client.PostAsJsonAsync("Review/", reviews);
+            if(response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<ReviewDto>();
+            return review;
+        }
+
+        //Edit Individual Review Details
+        public async Task<ReviewDto> EditReviewDetailsAsync(int Id)
+        {
+            var response = await _client.GetAsync("Review/edit/" + Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<ReviewDto>();
+            return review;
+        }
+
+        //Get Edit Delete
+        public async Task<ReviewDto> GetDeleteReviewAsync(int Id)
+        {
+            var response = await _client.GetAsync("Review/delete/" + Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<ReviewDto>();
+            return review;
+        }
+
+        public async Task<IEnumerable<ReviewDto>> GetReviewDetailsListAsync(int Id)
+        {
+            var response = await _client.GetAsync("Review/" + Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            response.EnsureSuccessStatusCode();
+            var review = await response.Content.ReadAsAsync<IEnumerable<ReviewDto>>();
+            return review;
+        }
+    }
+}
