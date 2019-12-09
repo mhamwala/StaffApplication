d5d5deaf3965675b41e52868e8f860cdbe5df307
Musa Hamwala II <musahamwala@icloud.com>
2019-12-09 16:48:54 +0000

    iniial working integration (docker compose)

--- ThreeAmigosStaff/ThreeAmigosStaff/Services/Product/ProductService.cs
+++ ThreeAmigosStaff/ThreeAmigosStaff/Services/Product/ProductService.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace ThreeAmigosProduct.Services
 {
     public class ProductService : IProductService
     {
         private readonly HttpClient _client;
+        private readonly ILogger _logger;
 
-        public ProductService(HttpClient client)
+        public ProductService(HttpClient client, ILogger<ProductService> logger)
         {
-            client.BaseAddress = new System.Uri("http://localhost:5001/");
+            client.BaseAddress = new System.Uri("http://manage-products-api/api/");
             client.Timeout = TimeSpan.FromSeconds(5);
             client.DefaultRequestHeaders.Add("Accept", "application/json");
             _client = client;
+            _logger = logger;
         }
 
         //Get all Product
         public async Task<IEnumerable<ProductDto>> GetProductAsync()
         {
-            var response = await _client.GetAsync("Product/");
+            _logger.LogWarning("FAIling to do stuff.");
+            Console.WriteLine("asjdhfjshd");
+            var response = await _client.GetAsync("products/");
             if(response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
             }
+            System.Console.WriteLine(response.StatusCode);
             response.EnsureSuccessStatusCode();
             var product = await response.Content.ReadAsAsync<IEnumerable<ProductDto>>();
             return product;
@@ -35,7 +41,7 @@ namespace ThreeAmigosProduct.Services
         //Get all Products
         public async Task<IEnumerable<ProductDto>> GetProductsAsync(int Id)
         {
-            var response = await _client.GetAsync("Product/" + Id);
+            var response = await _client.GetAsync("products/" + Id);
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
@@ -49,7 +55,7 @@ namespace ThreeAmigosProduct.Services
         //Get Individual Product Details
         public async Task<ProductDto> GetProductDetailsAsync(int Id)
         {
-            var response = await _client.GetAsync("Product/details/" + Id);
+            var response = await _client.GetAsync("products/details/" + Id);
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
@@ -62,7 +68,7 @@ namespace ThreeAmigosProduct.Services
         //Post New Product Member
         public async Task<ProductDto> PostProductAsync(ProductDto products)
         {
-            var response = await _client.PostAsJsonAsync("Product/", products);
+            var response = await _client.PostAsJsonAsync("products/", products);
             if(response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
@@ -75,7 +81,7 @@ namespace ThreeAmigosProduct.Services
         //Edit Individual Product Details
         public async Task<ProductDto> EditProductDetailsAsync(int Id)
         {
-            var response = await _client.GetAsync("Product/edit/" + Id);
+            var response = await _client.GetAsync("products/edit/" + Id);
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
@@ -88,7 +94,7 @@ namespace ThreeAmigosProduct.Services
         //Get Edit Delete
         public async Task<ProductDto> GetDeleteProductAsync(int Id)
         {
-            var response = await _client.GetAsync("Product/delete/" + Id);
+            var response = await _client.GetAsync("products/delete/" + Id);
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
                 return null;
