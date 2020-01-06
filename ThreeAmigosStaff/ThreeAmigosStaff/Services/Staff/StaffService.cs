8cd4cb4f4479f6a055598d6810bead7da40a8dbf
Musa Hamwala II <musahamwala@icloud.com>
2020-01-06 14:29:30 +0000

    fixing all routes

--- ThreeAmigosStaff/ThreeAmigosStaff/Services/Staff/StaffService.cs
+++ ThreeAmigosStaff/ThreeAmigosStaff/Services/Staff/StaffService.cs
@@ -14,7 +14,7 @@ namespace ThreeAmigosStaff.Services
 
         public StaffService(HttpClient client, ILogger<StaffService> logger)
         {
-            client.BaseAddress = new System.Uri("http://manage-staff-api/api/");
+            client.BaseAddress = new System.Uri("https://managestaffapi.azurewebsites.net/api/");
             client.Timeout = TimeSpan.FromSeconds(5);
             client.DefaultRequestHeaders.Add("Accept", "application/json");
             _client = client;
