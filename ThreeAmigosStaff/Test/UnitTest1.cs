e21f57fd403e0cf3cdaeaf8a39c381579750127e
Musa Hamwala II <musahamwala@icloud.com>
2020-01-10 13:07:17 +0000

    initial unit testing

---
+++ ThreeAmigosStaff/Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+using System;
+using ThreeAmigosCustomer.Controllers;
+using ThreeAmigosCustomer.Services;
+using Xunit;
+
+namespace Test
+{
+    public class CustomerControllerTest
+    {
+        CustomerController _customerController;
+        ICustomerService _customerService;
+
+        public CustomerControllerTest()
+        {
+            _customerService = new FakeCustomerService();
+            _customerController = new CustomerController(_customerService);
+        }
+
+        public class UnitTest1
+        {
+            [Fact]
+            public void Test1()
+            {
+                // Act
+                var okResult = _customerController.Index();
+
+                // Assert
+                Assert.Equal(1, 1);
+            }
+        }
+    }
+}
